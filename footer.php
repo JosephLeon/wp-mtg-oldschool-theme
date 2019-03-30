b486e865d3f3377e9381d7a00b1c759bab12f0cd
Joseph Leon <joseph.leon.9@gmail.com>
2019-03-30 00:55:13 -0400

    moar stuff

--- footer.php
+++ footer.php
@@ -1,18 +1,20 @@
     <footer>
-      <div class="">
-        <p>Copyright &#169; <?php echo date("Y"); ?></p>
-      </div>
-      <!-- <div class="">
-        <p>
-          Join us on <a href="#"><i class="fab fa-facebook-square"></i></a>
-        </p>
-      </div> -->
-      <div class="">
-        <nav class="group">
-          <?php wp_nav_menu(array(
-            'theme_location' => 'footerMenu'
-          )) ?>
-        </nav>
+      <div id="footer-content">
+        <div class="">
+          <p>Copyright &#169; <?php echo date("Y"); ?></p>
+        </div>
+        <!-- <div class="">
+          <p>
+            Join us on <a href="#"><i class="fab fa-facebook-square"></i></a>
+          </p>
+        </div> -->
+        <div class="">
+          <nav class="group">
+            <?php wp_nav_menu(array(
+              'theme_location' => 'footerMenu'
+            )) ?>
+          </nav>
+        </div>
       </div>
     </footer>
   <?php wp_footer(); ?>
--- index.php
+++ index.php
@@ -1,30 +1,21 @@
-<?php
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- */
+<?php get_header(); ?>
 
- get_header();
-?>
-<div class="main">
-  <?php
-    while(have_posts()) {
-      the_post(); ?>
-      <div class="post">
-        <h3>
-          <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-        </h3>
-        <div class="post-content">
-          <?php the_content(); ?>
+<section id="primary" class="content-area">
+  <main id="main" class="site-main">
+    <?php
+      while(have_posts()) {
+        the_post(); ?>
+        <div class="post">
+          <h3>
+            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+          </h3>
+          <div class="post-content">
+            <?php the_content(); ?>
+          </div>
         </div>
-      </div>
-    <?php }
-  ?>
-</div>
+      <?php }
+    ?>
+  </main>
+</section>
 
-<?php
-  get_footer();
+<?php get_footer();
